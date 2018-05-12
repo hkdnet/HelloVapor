b5abd2b4dc21ad80739991000dac3dfd3e376dd4
hkdnet <satoko.itse@gmail.com>
2018-05-12 23:54:34 +0900

    Add my middleware
    This will fail on booting... X<

---
+++ Sources/App/mymiddleware.swift
@@ -0,0 +1,32 @@
+import Vapor
+
+public final class MyMiddleware: Middleware, ServiceType {
+    /// See `ServiceType`.
+    public static func makeService(for worker: Container) throws -> MyMiddleware {
+        return try .default(environment: worker.environment, log: worker.make())
+    }
+    
+    /// Create a default `ErrorMiddleware`. Logs errors to a `Logger` based on `Environment`
+    /// and converts `Error` to `Response` based on conformance to `AbortError` and `Debuggable`.
+    ///
+    /// - parameters:
+    ///     - environment: The environment to respect when presenting errors.
+    ///     - log: Log destination.
+    public static func `default`(environment: Environment, log: Logger) -> MyMiddleware {
+        return .init()
+    }
+
+    public init() {
+    }
+    
+    /// See `Middleware`.
+    public func respond(to req: Request, chainingTo next: Responder) throws -> Future<Response> {
+        return try next.respond(to: req).map { res in
+            if req.http.headers.contains(name: "X-NYA-N") {
+                res.http.headers.add(name: "X-NYA-N", value: "RESPONSE")
+            }
+            return res
+        }
+    }
+}
+
